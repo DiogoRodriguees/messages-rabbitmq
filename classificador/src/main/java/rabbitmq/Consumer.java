aecf4c9d8bf6166bcb2222232bb3ee9e60c8db85
diogorodriguees <diogorodrigueslife@gmail.com>
2024-08-13 15:18:38 -0300

    :sparkles: feat: implement consumer

--- classificador/src/main/java/rabbitmq/Consumer.java
+++ classificador/src/main/java/rabbitmq/Consumer.java
@@ -7,16 +7,18 @@ import com.rabbitmq.client.DeliverCallback;
 import messages.Message;
 
 import java.io.IOException;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import messages.SpecificTopic;
 
 public class Consumer {
     private final static String QUEUE_NAME = "hello";
-    private final static ObjectMapper objectMapper = new ObjectMapper(); // Inicialize o ObjectMapper
 
     public void execute() {
         try {
+             ObjectMapper objectMapper = new ObjectMapper(); // Inicialize o ObjectMapper
             System.out.println("Starting connection");
-            // Configurar a conexão com o RabbitMQ
+//             Configurar a conexão com o RabbitMQ
             ConnectionFactory factory = new ConnectionFactory();
             factory.setHost("localhost");
             Connection connection = factory.newConnection();
@@ -27,11 +29,6 @@ public class Consumer {
             System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
             // Definir o callback que será executado quando uma mensagem for recebida
-//            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-//                String message = new String(delivery.getBody(), "UTF-8");
-//                System.out.println(" [x] Received '" + message + "'");
-//            };
-            // Definir o callback que será executado quando uma mensagem for recebida
             DeliverCallback deliverCallback = (consumerTag, delivery) -> {
                 String messageBody = new String(delivery.getBody(), "UTF-8");
 
@@ -40,6 +37,10 @@ public class Consumer {
                     Message message = objectMapper.readValue(messageBody, Message.class);
 //                    System.out.println(" [x] Received '" + message + "'");
                     System.out.println("Topic:" + message.GenericTopic.Name);
+                    for (int i = 0; i< message.SpecificTopics.size(); i++) {
+                        this.sendTopic(message.SpecificTopics.get(i));
+                    }
+
                 } catch (IOException e) {
                     System.err.println("Failed to deserialize message: " + e.getMessage());
                 }
@@ -51,4 +52,22 @@ public class Consumer {
 
         }
     }
+
+    public void sendTopic(SpecificTopic specificTopic) {
+        try{
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost");
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+            channel.queueDeclare(specificTopic.Name, false, false, false, null);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            byte[] msg = objectMapper.writeValueAsBytes(specificTopic);
+
+            channel.basicPublish("", specificTopic.Name, null, msg);
+            System.out.println(" [x] Sent '" + specificTopic.Name + "'");
+        }catch (Exception e){
+            System.out.println("Deu ruim");
+        }
+   }
 }
