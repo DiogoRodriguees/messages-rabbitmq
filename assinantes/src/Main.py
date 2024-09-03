968d395e0115d0dc364233fc2068fb246f085988
diogorodriguees <diogorodrigueslife@gmail.com>
2024-09-02 22:20:24 -0300

    :sparkles: assinantes: add description comments

--- assinantes/src/Main.py
+++ assinantes/src/Main.py
@@ -3,13 +3,16 @@ import sys
 
 
 amount_params = sys.argv.__len__()
-sys.argv.pop(0)
-params = list(sys.argv)
+sys.argv.pop(0)  # remove o primeiro parametro (nome do programa)
+params = list(sys.argv)  # tranforma os demais parametros em uma lista
 
+# verifica existencia de ao menos um parametro
 if amount_params > 1:
+    # para cada parametro, cria os binds nas exchanges
     for param in params:
-        param_splited = param.split(":")
-        exchange = param_splited[0]
+        param_splited = param.split(":")  # separa o topico geral do especifico
+        exchange = param_splited[0]  # topico principal é o nome da exchange
+        # topcios especifcos sao os nomes das routing keys
         routing_keys = param_splited[1].split(",")
 
         rabbitmq = RabbitMQ()
